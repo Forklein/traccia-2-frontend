4de57d8596ef24cabd89d15386334ca1907f4290
Forklein <giuseppepisani2@gmail.com>
2022-05-14 18:22:26 +0200

    Refactoring code

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-//!Info pdv
-// - Piazza Luigi di Savoia 1 	- Milano
-// - Via Togliatti, 2 	- Milano
-// - piazza Venticinque Aprile 	- Milano
-// - via Emilia Ovest 1480 	- Modena
-// - via Emilia Est 981 	- Modena
-// - via Orabona 4 	- Bari
-// - piazza Ferrarese 28 - Bari	
-
-
-
-
 const mapOptions = {
     center: [42.36, 12.02],
     zoom: 5.5
@@ -20,30 +8,51 @@ const map = new L.map('map', mapOptions);
 const layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 map.addLayer(layer);
 
+//Construct object markers
 
-//Milan
-const firstMilan = [45.4862737, 9.2063373];
-const streetFirstMilan = 'Piazza Luigi di Savoia, 1 - Milano';
-const secondMilan = [45.5299763, 9.3666279];
-const streetSecondMilan = 'Via Togliatti, 2 - Milano';
-const thirdMilan = [45.284973, 9.111306];
-//Modena
-const firstModena = [44.64763, 10.85401];
-const secondModena = [44.6345866, 10.9552156];
-//Bari
-const firstBari = [41.1080290, 16.8805320];
-const secondBari = [41.126959, 16.87183968931251];
+const markers = [
+    {
+        coord: [45.4862737, 9.2063373],
+        street: 'Piazza Luigi di Savoia, 1 - Milano',
+        path: './assets/img/img1.png',
+        link: '',
+    },
+    {
+        coord: [45.5299763, 9.3666279],
+        street: 'Via Togliatti, 2 - Milano',
+    },
+    {
+        coord: [45.284973, 9.111306],
+        street: 'Piazza 25 Aprile - Milano'
+    },
+    {
+        coord: [44.64763, 10.85401],
+        street: 'Via Emilia Ovest, 1480 - Modena'
+    },
+    {
+        coord: [44.6345866, 10.9552156],
+        street: 'Via Emilia Est, 981 - Modena'
+    },
+    {
+        coord: [41.1080290, 16.8805320],
+        street: 'Via Orabona, 4 - Bari'
+    },
+    {
+        coord: [41.126959, 16.87183968931251],
+        street: 'Piazza Ferrarese, 28 - Bari'
+    },
+];
 
-const coords = [firstMilan, secondMilan, thirdMilan, firstModena, secondModena, firstBari, secondBari];
-
-
-coords.forEach((coord, i) => {
-    const marker = new L.Marker(coord, { draggable: true });
-    const src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Stack_Exchange_logo_and_wordmark.svg/375px-Stack_Exchange_logo_and_wordmark.svg.png';
+markers.forEach((marker) => {
+    const tempMarker = new L.Marker(marker.coord, { draggable: true });
     const popupContent = document.createElement("div");
-    popupContent.innerHTML = `<img src="${src}" /> <a href="">Google Map Link</a>`;
-    marker.addTo(map).bindPopup(popupContent);
+    const path = './assets/img/img1.png';
+    popupContent.innerHTML = `<div class="background" style="background-image: url('${marker.path}');"></div><a href="${marker.url}">${marker.street}</a>`;
+    tempMarker.addTo(map).bindPopup(popupContent);
 });
+
+
+
 
 
 
